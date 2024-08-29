a6dfa118ac2fc1aa2deb73e63bbac55311583fe1
darkmage <165251+mikedesu@users.noreply.github.com>
2024-08-29 00:47:58 -0500

    moved all Sprite methods into Sprite

--- Sprite.h
+++ Sprite.h
@@ -43,9 +43,32 @@ private:
   movement_type movement = MOVEMENT_TYPE_NONE;
 
 public:
-  Sprite(Texture2D &texture, const unsigned int anim_frames,
-         const unsigned int contexts, const float x, const float y, const int w,
-         const int h, sprite_type t);
+  Sprite(Texture2D &t, const unsigned int frames, const unsigned int c,
+         const float x, const float y, const int w, const int h,
+         sprite_type spritetype) {
+
+    texture = t;
+    anim_frames = frames;
+    contexts = c;
+    width = w;
+    height = h;
+    set_scale(1.0f);
+    init_rects(w, h);
+    origin = (Vector2){0, 0};
+    dest.x = x;
+    dest.y = y;
+    current_frame = 0;
+    velocity = (Vector2){0, 0};
+    is_marked_for_deletion = false;
+    is_animating = false;
+    is_flipped = false;
+    type = spritetype;
+    is_spinning = false;
+    velocity.x = 0;
+    velocity.y = 0;
+    acceleration.x = 0;
+    acceleration.y = 0;
+  }
 
   ~Sprite() {}
 
