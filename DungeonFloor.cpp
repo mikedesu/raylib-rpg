83704eedbc97e2f4b4c0b48adc07fc727b6670f3
darkmage <165251+mikedesu@users.noreply.github.com>
2024-08-30 23:39:44 -0500

    moved dungeonfloor code into .cpp in prep for conversion to struct

--- DungeonFloor.cpp
+++ DungeonFloor.cpp
@@ -1,5 +1,99 @@
 #include "DungeonFloor.h"
 
+DungeonFloor::DungeonFloor() {
+    set_tile_type_all(TILE_FLOOR_BASIC);
+    for(int i = 0; i < gridsize; i++) {
+        set_tile_type(i, 0, TILE_FLOOR_WOOD);
+        set_tile_type(i, 1, TILE_FLOOR_DIRT);
+    }
+    set_tile_type(0, 0, TILE_FLOOR_UPSTAIRS);
+    set_tile_type(7, 7, TILE_FLOOR_DOWNSTAIRS);
+}
+DungeonFloor::~DungeonFloor() { }
+
+void DungeonFloor::set_tile_type(const int col, const int row, const TileType value) {
+    if(row < 0 || row >= gridsize || col < 0 || col >= gridsize) {
+        return;
+    }
+    grid[col][row].set_type(value);
+}
+
+void DungeonFloor::set_tile_type_all(const TileType value) {
+    for(int i = 0; i < gridsize; i++) {
+        for(int j = 0; j < gridsize; j++) {
+            set_tile_type(i, j, value);
+        }
+    }
+}
+
+const int DungeonFloor::get_gridsize() const {
+    return gridsize;
+}
+const TileType DungeonFloor::get_tile_type(const int col, const int row) const {
+    if(row < 0 || row >= gridsize || col < 0 || col >= gridsize) {
+        return TILE_NONE;
+    }
+    return grid[col][row].get_type();
+}
+const Vector2 DungeonFloor::get_entity_position(const EntityId id) {
+    if(entity_positions.find(id) == entity_positions.end()) {
+        return Vector2{-1, -1};
+    }
+    return entity_positions[id];
+}
+
+Tile& DungeonFloor::get_tile_by_col_row(const int col, const int row) {
+    return grid[col][row];
+}
+Tile& DungeonFloor::get_tile_by_vec(const Vector2 position) {
+    return grid[(int)position.x][(int)position.y];
+}
+
+const vector<EntityId>& DungeonFloor::get_entities(const int col, const int row) const {
+    return grid[col][row].get_entities();
+}
+
+void DungeonFloor::remove_entity(const EntityId id) {
+    remove_entity_from_tile(id, get_entity_position(id));
+}
+
+void DungeonFloor::remove_entity_from_tile(const EntityId id, const Vector2 position) {
+    grid[(int)position.x][(int)position.y].remove_entity(id);
+    entity_positions.erase(id);
+    entities.erase(id);
+}
+
+const EntityType DungeonFloor::get_entity_type(const EntityId id) const {
+
+    if(entities.find(id) == entities.end()) {
+        return ENTITY_NONE;
+    }
+    return entities.at(id)->get_type();
+}
+
+const bool DungeonFloor::loc_contains_entity_type(const Vector2 loc, const EntityType type) const {
+    const vector<EntityId>& entities = get_entities(loc.x, loc.y);
+    auto pred = [this, type](EntityId id) { return get_entity_type(id) == type; };
+    return find_if(entities.begin(), entities.end(), pred) != entities.end();
+}
+
+const string DungeonFloor::get_entity_name(const EntityId id) const {
+    return entities.at(id)->get_name();
+}
+
+void DungeonFloor::add_entity_at(shared_ptr<Entity> entity, const Vector2 position) {
+    EntityId id = entity->get_id();
+    int x = (int)position.x;
+    int y = (int)position.y;
+    if(entity_positions.find(id) == entity_positions.end()) {
+        entity_positions[id] = position;
+        entities[id] = entity;
+        grid[x][y].add_entity(id);
+    } else {
+        move_entity_to_tile(id, position);
+    }
+}
+
 const bool DungeonFloor::move_entity_to_tile(EntityId id, const Vector2 t_pos) {
     // can only move under certain conditions:
     // the entity is in this tile
--- DungeonFloor.h
+++ DungeonFloor.h
@@ -18,99 +18,33 @@ private:
     unordered_map<EntityId, shared_ptr<Entity>> entities;
 
 public:
-    DungeonFloor() {
-        set_tile_type_all(TILE_FLOOR_BASIC);
-        for(int i = 0; i < gridsize; i++) {
-            set_tile_type(i, 0, TILE_FLOOR_WOOD);
-            set_tile_type(i, 1, TILE_FLOOR_DIRT);
-        }
-        set_tile_type(0, 0, TILE_FLOOR_UPSTAIRS);
-        set_tile_type(7, 7, TILE_FLOOR_DOWNSTAIRS);
-    }
-    ~DungeonFloor() { }
+    DungeonFloor();
+    ~DungeonFloor();
 
-    void set_tile_type(const int col, const int row, const TileType value) {
-        if(row < 0 || row >= gridsize || col < 0 || col >= gridsize) {
-            return;
-        }
-        grid[col][row].set_type(value);
-    }
+    void set_tile_type(const int col, const int row, const TileType value);
 
-    void set_tile_type_all(const TileType value) {
-        for(int i = 0; i < gridsize; i++) {
-            for(int j = 0; j < gridsize; j++) {
-                set_tile_type(i, j, value);
-            }
-        }
-    }
+    void set_tile_type_all(const TileType value);
 
-    const int get_gridsize() const {
-        return gridsize;
-    }
-    const TileType get_tile_type(const int col, const int row) const {
-        if(row < 0 || row >= gridsize || col < 0 || col >= gridsize) {
-            return TILE_NONE;
-        }
-        return grid[col][row].get_type();
-    }
-    const Vector2 get_entity_position(const EntityId id) {
-        if(entity_positions.find(id) == entity_positions.end()) {
-            return Vector2{-1, -1};
-        }
-        return entity_positions[id];
-    }
+    const int get_gridsize() const;
+    const TileType get_tile_type(const int col, const int row) const;
+    const Vector2 get_entity_position(const EntityId id);
 
-    Tile& get_tile_by_col_row(const int col, const int row) {
-        return grid[col][row];
-    }
-    Tile& get_tile_by_vec(const Vector2 position) {
-        return grid[(int)position.x][(int)position.y];
-    }
+    Tile& get_tile_by_col_row(const int col, const int row);
+    Tile& get_tile_by_vec(const Vector2 position);
 
-    const vector<EntityId>& get_entities(const int col, const int row) const {
-        return grid[col][row].get_entities();
-    }
+    const vector<EntityId>& get_entities(const int col, const int row) const;
 
-    void remove_entity(const EntityId id) {
-        remove_entity_from_tile(id, get_entity_position(id));
-    }
+    void remove_entity(const EntityId id);
 
-    void remove_entity_from_tile(const EntityId id, const Vector2 position) {
-        grid[(int)position.x][(int)position.y].remove_entity(id);
-        entity_positions.erase(id);
-        entities.erase(id);
-    }
+    void remove_entity_from_tile(const EntityId id, const Vector2 position);
 
-    const EntityType get_entity_type(const EntityId id) const {
+    const EntityType get_entity_type(const EntityId id) const;
 
-        if(entities.find(id) == entities.end()) {
-            return ENTITY_NONE;
-        }
-        return entities.at(id)->get_type();
-    }
+    const bool loc_contains_entity_type(const Vector2 loc, const EntityType type) const;
 
-    const bool loc_contains_entity_type(const Vector2 loc, const EntityType type) const {
-        const vector<EntityId>& entities = get_entities(loc.x, loc.y);
-        auto pred = [this, type](EntityId id) { return get_entity_type(id) == type; };
-        return find_if(entities.begin(), entities.end(), pred) != entities.end();
-    }
+    const string get_entity_name(const EntityId id) const;
 
-    const string get_entity_name(const EntityId id) const {
-        return entities.at(id)->get_name();
-    }
-
-    void add_entity_at(shared_ptr<Entity> entity, const Vector2 position) {
-        EntityId id = entity->get_id();
-        int x = (int)position.x;
-        int y = (int)position.y;
-        if(entity_positions.find(id) == entity_positions.end()) {
-            entity_positions[id] = position;
-            entities[id] = entity;
-            grid[x][y].add_entity(id);
-        } else {
-            move_entity_to_tile(id, position);
-        }
-    }
+    void add_entity_at(shared_ptr<Entity> entity, const Vector2 position);
 
     const bool move_entity_to_tile(EntityId id, const Vector2 position);
 };
