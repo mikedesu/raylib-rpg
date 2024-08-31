bc57b5b653431ebb787b7e31f5f956d9fc3bcbe3
darkmage <165251+mikedesu@users.noreply.github.com>
2024-08-31 00:20:38 -0500

    update script

--- update_assets.sh
+++ update_assets.sh
@@ -3,3 +3,5 @@
 rm -rf projectrpg-img.zip;
 # then zip up the new assets
 zip -r projectrpg-img.zip img audio fonts title_textures.txt game_textures.txt gameover_textures.txt;
+# then copy the new zip to the website folder
+cp projectrpg-img.zip ../mikedesu.github.io/.;
